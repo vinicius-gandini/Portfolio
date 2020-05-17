564c7b3bf2599949399478a9aa8e46570c8dceb1
vinicius-gandini <viniciusgandini.goncalves@hotmail.com>
2020-05-17 17:45:49 -0300

    responsive navbar working good

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 document.addEventListener('DOMContentLoaded', function (){
 
-    document.querySelector('#topbar').addEventListener('click', (e) => {
-
         const list1 = document.querySelector('#list1');
         const list2 = document.querySelector('#list2');
-/*
-        if (list1.className === 'nav-itens' || list2.className === 'nav-itens'){
-            list1.className += ' responsive1';
-            list2.className += ' responsive2';
+        const nav = document.querySelector('#nav');
+        const home = document.querySelector('#menu');
+        const bar = document.querySelector('#topbar');
+
+    document.querySelector('#topbar').addEventListener('click', (e) => {
+
+        let menu = document.querySelector('#float-menu');
+        let text = document.createTextNode("");
+        if (menu.className === '') {
+            menu.className += 'responsive';
+            list1.className += ' responsiveNav';
+            list2.className += ' responsiveNav';
+            menu.appendChild(text);
+            menu.appendChild(list1);
+            menu.appendChild(list2);
         }
-            
         else {
+            menu.removeChild(list1);
+            menu.removeChild(list2);
+            menu.className = '';
             list1.className = 'nav-itens';
             list2.className = 'nav-itens';
+            nav.appendChild(list1);
+            nav.appendChild(home);
+            nav.appendChild(list2);
+            nav.appendChild(bar);
         }
-*/
-
-        const menu = document.createElement('div');
-        menu.className += ' responsive'
-        menu.appendChild(list1)
-        menu.appendChild(list2)
+        
         
     })
 });
