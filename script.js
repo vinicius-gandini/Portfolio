ac4bbb7e5157087dba5ba8b28da507f1682dcf49
vinicius-gandini <viniciusgandini.goncalves@hotmail.com>
2020-05-19 20:16:51 -0300

    Change JS to make navbar responsivity working fine

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-document.addEventListener('DOMContentLoaded', function (){
+document.addEventListener('DOMContentLoaded', function () {
 
     document.querySelector('#topbar').addEventListener('click', (e) => {
 
-       let nav = document.querySelector('#list1');
-       nav.classList.toggle('responsive');
+        let firstUl = document.querySelector('#list1');
+        let firstLi = document.querySelector('#link1');
+        let secLi = document.querySelector('#link2');
+        let SecUl = document.querySelector('#list2');
 
-       let nav2 = document.querySelector('#list2');
-       nav2.classList.toggle('responsive2');
+        if (firstUl.className === 'nav-itens') {
+            firstUl.classList.toggle('responsive');
+            firstUl.appendChild(firstLi);
+            firstUl.appendChild(secLi);
+        }
+        else {
+            firstUl.classList.remove('responsive');
+            SecUl.appendChild(firstLi);
+            SecUl.appendChild(secLi);
+        }
     })
 });
 
